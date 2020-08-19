a209802526dfcba974ed5dc8db91cbbece7f1e18
yokogawa-k <yokogawa-k@klab.com>
2020-08-19 13:09:04 +0900

    [CloudFormation Guard] Update v0.6.0-beta

--- cfn-guard.rb
+++ cfn-guard.rb
@@ -4,9 +4,9 @@ class CfnGuard < Formula
 
   # curl -s https://api.github.com/repos/aws-cloudformation/cloudformation-guard/tags | jq .[0]
   # curl -sL https://github.com/aws-cloudformation/cloudformation-guard/archive/$(curl -s https://api.github.com/repos/aws-cloudformation/cloudformation-guard/tags | jq -r .[0].name).tar.gz | sha256sum
-  VERSION = "v0.5.2-beta"
+  VERSION = "v0.6.0-beta"
   url "https://github.com/aws-cloudformation/cloudformation-guard/archive/#{VERSION}.tar.gz"
-  sha256 "05f48b30708c053367d5792b96af64914e149d7094a5b4c92f4d67d998e473b8"
+  sha256 "db27dd65572bfdc16894be3d9084248370ac1392d2edc998b1e72c7cd2f13322"
 
   head "https://github.com/aws-cloudformation/cloudformation-guard.git"
 
